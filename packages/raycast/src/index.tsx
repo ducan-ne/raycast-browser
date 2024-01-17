9534df8845bebb2fb103c4c7fc10d52f797b6163
An <me@clgt.io>
2024-01-18 00:50:52 +0700

    feat: update sample extension

--- packages/raycast/src/index.tsx
+++ packages/raycast/src/index.tsx
@@ -4,34 +4,58 @@ import { browser } from 'browser-api/node'
 import { useState } from 'react'
 
 export default function Command() {
-  const {push} = useNavigation()
-  const tabs = usePromise(() => browser.tabs.query({}), [])
-  const [detail, setDetail] = useState('')
+  const { push } = useNavigation()
+  const tabs = usePromise(
+    () => browser.tabs.query({})
+  )
   const [isLoading, setIsLoading] = useState(false)
+  const cookies = usePromise(
+    () => browser.cookies.getAll({
+      url: 'https://www.facebook.com'
+    })
+  )
+  // cookies.data[0].
 
   return (
     <List isLoading={tabs.isLoading || isLoading}>
-      {tabs.data?.map(tab => (
-        <List.Item
-          icon={tab.favIconUrl}
-          key={tab.id}
-          title={tab.title || '-'}
-          actions={
-            <ActionPanel>
-              <Action
-                title="Show Details"
-                onAction={async() => {
-                  setIsLoading(true)
-                  push(
-                    <Detail markdown={`# document.title: ${await browser.executeScript(tab.id!, () => document.title)}`} />
-                  )
-                  setIsLoading(false)
-                }}
-              />
-            </ActionPanel>
-          }
-        />
-      ))}
+      {tabs.data?.map(tab => {
+        const actions = (
+          <ActionPanel>
+            <Action
+              title="Show Details"
+              onAction={async () => {
+                setIsLoading(true)
+                push(
+                  <Detail
+                    markdown={`
+# document.title: ${await browser.executeScript(tab.id!, () => document.title)}
+# github title: ${await browser.executeScript(tab.id!, () => document.querySelector('[class="f4 color-fg-muted col-md-6 mx-auto"]')?.textContent)}
+                    `.trim()}
+                  />
+                )
+                setIsLoading(false)
+              }}
+            />
+            <Action
+              title="Set background to red color"
+              onAction={async () => {
+                await browser.executeScript(tab.id!, () => {
+                  document.body.style.background = 'red'
+                })
+              }}
+            />
+          </ActionPanel>
+        )
+
+        return (
+          <List.Item
+            icon={tab.favIconUrl}
+            key={tab.id}
+            title={tab.title || '-'}
+            actions={actions}
+          />
+        )
+      })}
     </List>
   )
 }
